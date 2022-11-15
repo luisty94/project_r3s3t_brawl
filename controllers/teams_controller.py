4aecbabb19e40c83c185a203f11b082af7f26904
luisty94 <luistrigoyuste94@gmail.com>
2022-11-15 15:22:53 +0000

    sixth commit

--- controllers/teams_controller.py
+++ controllers/teams_controller.py
@@ -13,3 +13,32 @@ def games():
 @teams_blueprint.route("/teams/new")
 def new_team():
     return render_template("teams/new.html")
+
+@teams_blueprint.route("/teams", methods=["POST"])
+def create_team():
+    name = request.form["name"]
+    new_team = Team(name)
+    team_repository.save(new_team)
+    return redirect("/teams")
+
+@teams_blueprint.route("/teams/<id>/edit")
+def edit_team(id):
+    team = team_repository.select(id)
+    return render_template('teams/edit.html', team=team)
+
+@teams_blueprint.route("/teams/<id>", methods=["POST"])
+def update_team(id):
+    name = request.form["name"]
+    team = Team(name, id)
+    team_repository.update(team)
+    return redirect("/teams")
+
+@teams_blueprint.route("/teams/<id>/delete", methods=["POST"])
+def delete_team(id):
+    team_repository.delete(id)
+    return redirect("/teams")
+
+@teams_blueprint.route("/teams/<id>")
+def show_team(id):
+    team = team_repository.select(id)
+    return render_template('team/show.html', team = team)
